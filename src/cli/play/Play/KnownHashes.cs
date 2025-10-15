a4df64d9d5a913a558eacd7ccb547f61aa463f74
jimmy-dsi <15963851+jimmy-dsi@users.noreply.github.com>
2025-10-15 00:17:38 -0700

    update KnownHashes.cs

--- src/cli/play/Play/KnownHashes.cs
+++ src/cli/play/Play/KnownHashes.cs
@@ -5,11 +5,11 @@ using Jimbl;
 
 public static class KnownHashes {
 	static string[] windows = [
-	
+		"e8f1e9f0eb11b571336d6a16d7585e91c90bced734d47122810ac173589d20ff" // v0.1.0 (x86-64)
 	];
 	
 	static string[] linux = [
-	
+		"5733859762265cfb29f9314f7df747027f12e6913f7dd44800f64ab3c3ce39fe" // v0.1.0 (x86-64)
 	];
 	
 	public static string[] Set {
