c3ca53357ec7ca655d685297dd83aeb5446b178e
jimmy-dsi <15963851+jimmy-dsi@users.noreply.github.com>
2025-10-25 01:39:29 -0700

    Pipeline2: echo and FIR now respond to channel muting

--- src/core/echo.zig
+++ src/core/echo.zig
@@ -40,6 +40,8 @@ fn read(s: *DSPStateInternal, comptime channel: u1, aram_echo_0: [*]u8, aram_ech
     const samp_u16: u16 = @as(u16, lo) | @as(u16, hi) << 8;
     const samp_i16: i16 = @bitCast(samp_u16);
 
+    s.pipeline_2.trigger_echo_read(channel, samp_i16);
+
     if (channel == 0) {
         e._history_left[e._history_offset] = samp_i16 >> 1;
     }
@@ -92,6 +94,8 @@ pub fn step_a(s: *DSPStateInternal, aram_echo_0: [*]u8, aram_echo_1: [*]u8, fir_
     const left:  i17 = calc_fir(s, 0, 0, fir_0);
     const right: i17 = calc_fir(s, 1, 0, fir_0);
 
+    s.pipeline_2.set_fir_coef(0, fir_0);
+
     e._input_left  = left;
     e._input_right = right;
 }
@@ -105,6 +109,9 @@ pub fn step_b(s: *DSPStateInternal, aram_echo_0: [*]u8, aram_echo_1: [*]u8, fir_
 
     left  +%= calc_fir(s, 0, 2, fir_2);
     right +%= calc_fir(s, 1, 2, fir_2);
+
+    s.pipeline_2.set_fir_coef(1, fir_1);
+    s.pipeline_2.set_fir_coef(2, fir_2);
 
     e._input_left  +%= left;
     e._input_right +%= right;
@@ -125,6 +132,10 @@ pub fn step_c(s: *DSPStateInternal, fir_3: i8, fir_4: i8, fir_5: i8) void {
     left  +%= calc_fir(s, 0, 5, fir_5);
     right +%= calc_fir(s, 1, 5, fir_5);
 
+    s.pipeline_2.set_fir_coef(3, fir_3);
+    s.pipeline_2.set_fir_coef(4, fir_4);
+    s.pipeline_2.set_fir_coef(5, fir_5);
+
     e._input_left  +%= left;
     e._input_right +%= right;
 }
@@ -142,6 +153,9 @@ pub fn step_d(s: *DSPStateInternal, fir_6: i8, fir_7: i8) void {
     const fir7_l_i16: i16 = @truncate(calc_fir(s, 0, 7, fir_7));
     const fir7_r_i16: i16 = @truncate(calc_fir(s, 1, 7, fir_7));
 
+    s.pipeline_2.set_fir_coef(6, fir_6);
+    s.pipeline_2.set_fir_coef(7, fir_7);
+
     left  = @as(i17,  left_i16) +% @as(i17, fir7_l_i16);
     right = @as(i17, right_i16) +% @as(i17, fir7_r_i16);
 
@@ -156,7 +170,7 @@ pub fn step_e(s: *DSPStateInternal, mvoll: i8, evoll: i8, efb: i8) void {
 
     p2.mvol_left = mvoll;
     p2.evol_left = evoll;
-    // TODO: Set feedback
+    p2.echo.feedback = efb;
 
     // Store echo left output for next clock tick
     s.__echo_out_left = output(s, 0, mvoll, evoll);
@@ -200,8 +214,15 @@ pub fn step_g(s: *DSPStateInternal, echo_readonly_flg: u1) void {
 
 pub fn step_h(s: *DSPStateInternal, aram_echo_0: [*]u8, aram_echo_1: [*]u8, edl: u4, esa: u8, echo_readonly_flg: u1) void {
     var e = &s._echo;
+    const p2 = s.pipeline_2;
 
     e._esa_page = esa;
+
+    p2.echo.delay = edl;
+    p2.echo.esa = esa;
+    p2.echo.readonly = echo_readonly_flg == 1;
+
+    p2.prepare_next_output();
 
     // Update buffer size based on edl once the current echo read offset wraps back to zero
     if (e._offset == 0) {
--- src/core/pipeline_2.zig
+++ src/core/pipeline_2.zig
@@ -1,3 +1,5 @@
+const std = @import("std");
+
 // Alternate render pipeline - This is used when any modifications are made in regard to player output
 // i.e. Channel disables, sample rate, interpolation/echo settings, etc.
 // This all goes through a separate pipeline so as not to interfere with emulation
@@ -61,13 +63,34 @@ pub const Pipeline2 = struct {
     };
 
     pub const Echo = struct {
+        readonly: bool = false,
+
+        esa: u8 = 0,
         delay: u4 = 0,
         feedback: i8 = 0,
 
         fir: [8]i8 = [_]i8{0} ** 8,
         eon: u8 = 0,
 
-        shadow_buffer: [0x2000]f64 = [_]f64{0} ** 0x2000
+        aram_record: [0x1_0000]?u8 = [_]?u8{null} ** 0x1_0000, // Records ARAM values written by the SMP, or uninited DSP buffer values
+        shadow_buffer_a: [0x8000]f64 = [_]f64{0} ** 0x8000,    // Main buffer used by this second pipeline - can be written through from ARAM writes
+        shadow_buffer_b: [0x8000]f64 = [_]f64{0} ** 0x8000,    // Similar to above, used when double echo-delay is enabled
+
+        history_left:  [8]f64 = [_]f64{0} ** 8,
+        history_right: [8]f64 = [_]f64{0} ** 8,
+
+        history_idx: u8 = 0,
+
+        read_head: u32 = 0,
+
+        out_left:  [3]f64 = [_]f64{0} ** 3,
+        out_right: [3]f64 = [_]f64{0} ** 3,
+
+        write_left:  [3]f64 = [_]f64{0} ** 3,
+        write_right: [3]f64 = [_]f64{0} ** 3,
+
+        _cur_edl: u4 = 0,
+        _cur_esa: u8 = 0,
     };
 
     enabled: bool = false,
@@ -108,6 +131,59 @@ pub const Pipeline2 = struct {
         self.check_settings();
     }
 
+    pub inline fn write_through(self: *Pipeline2, aram_addr: u16, byte_val: u8) void {
+        var e = &self.echo;
+        e.aram_record[aram_addr] = byte_val;
+
+        var samp_u16:     u16 = undefined;
+        var samp:         i16 = undefined;
+        var shadow_index: u32 = undefined;
+
+        if (aram_addr % 2 == 0) {
+            samp_u16 = @as(u16, e.aram_record[aram_addr].?) | @as(u16, e.aram_record[aram_addr +% 1] orelse 0) << 8;
+            samp     = @bitCast(samp_u16);
+
+            shadow_index = @as(u32, @divFloor(aram_addr, 2));
+        }
+        else {
+            samp_u16 = @as(u16, e.aram_record[aram_addr -% 1] orelse 0) | @as(u16, e.aram_record[aram_addr].?) << 8;
+            samp     = @bitCast(samp_u16);
+
+            shadow_index = @as(u32, @divFloor(aram_addr -% 1, 2));
+        }
+
+        const f_sample: f64 = i16_to_f64(samp);
+        e.shadow_buffer_a[shadow_index] = f_sample;
+        e.shadow_buffer_b[shadow_index] = f_sample;
+    }
+
+    pub inline fn trigger_echo_read(self: *Pipeline2, comptime channel: u1, sample: i16) void {
+        var e = &self.echo;
+
+        const head: u32 = e.read_head +% @as(u32, channel);
+
+        var aram_addr: u16 = self.esa_base_aram();
+        aram_addr +%= @intCast(head * 2);
+
+        const buffer_index: u32 = (self.esa_base_index() +% head) % 0x8000;
+
+        // Carry over data from the DSP echo buffer if we're attempting to read from an uninitialized area of the shadow buffer
+        if (e.aram_record[aram_addr] == null or e.aram_record[aram_addr +% 1] == null) {
+
+            e.aram_record[aram_addr]      = @intCast(sample & 0xFF);
+            e.aram_record[aram_addr +% 1] = @intCast(sample >>   8);
+
+            const f_sample: f64 = i16_to_f64(sample);
+            e.shadow_buffer_a[buffer_index] = f_sample;
+            e.shadow_buffer_b[buffer_index] = f_sample;
+        }
+    }
+
+    pub inline fn set_fir_coef(self: *Pipeline2, comptime fir_idx: u3, value: i8) void {
+        var e = &self.echo;
+        e.fir[fir_idx] = value;
+    }
+
     pub inline fn voice_output(self: *Pipeline2, index: u3, out: []const i16, vol: i8, comptime channel: u1) void {
         const v = &self.voice[index];
 
@@ -129,20 +205,36 @@ pub const Pipeline2 = struct {
     }
 
     pub fn output(self: *Pipeline2) void {
+        var e = &self.echo;
+
         for (0..self.samples_queued) |i| {
             // TODO: Consider alternate output formats for mixing
             var sum_left:  i32 = 0;
             var sum_right: i32 = 0;
 
+            var e_sum_left:  i32 = 0;
+            var e_sum_right: i32 = 0;
+
             for (0..8) |c| {
+                const cc: u3 = @intCast(c);
+
                 if (self.settings.channels_enabled[c]) {
                     const v = &self.voice[c];
 
                     const left  = f64_to_i16(v.buffer_left[i]);
                     const right = f64_to_i16(v.buffer_right[i]);
+                    const voice_left:  i32 = @as(i32, left)  * @as(i32, v.vol_left)  >> 7;
+                    const voice_right: i32 = @as(i32, right) * @as(i32, v.vol_right) >> 7;
 
-                    sum_left  +%= @as(i32, left)  * @as(i32, v.vol_left)  >> 7;
-                    sum_right +%= @as(i32, right) * @as(i32, v.vol_right) >> 7;
+                    sum_left  +%= voice_left;
+                    sum_right +%= voice_right;
+
+                    if (e.eon & (@as(u8, 1) << cc) != 0) { // If echo enabled for channel
+                        e_sum_left  +%= voice_left;
+                        e_sum_right +%= voice_right;
+                        e_sum_left  = clamp_i16(i32, e_sum_left);
+                        e_sum_right = clamp_i16(i32, e_sum_right);
+                    }
 
                     sum_left  = clamp_i16(i32, sum_left);
                     sum_right = clamp_i16(i32, sum_right);
@@ -152,8 +244,96 @@ pub const Pipeline2 = struct {
             sum_left  = trunc_i16(i32, sum_left  * self.mvol_left  >> 7);
             sum_right = trunc_i16(i32, sum_right * self.mvol_right >> 7);
 
-            self.dac_left[i]  = i16_to_f64(@intCast(sum_left));
-            self.dac_right[i] = i16_to_f64(@intCast(sum_right));
+            // TODO: Consider case when multiple samples will be output at once for echo - Also consider double-echo mode
+            const buffer_index: u32 = self.esa_base_index() +% e.read_head;
+
+            e.history_left[e.history_idx]  = e.shadow_buffer_a[buffer_index];
+            e.history_right[e.history_idx] = e.shadow_buffer_a[buffer_index +% 1];
+
+            e.history_idx = (e.history_idx + 1) % 8;
+
+            // Perform FIR calculation (TODO: Consider 24-bit/32-bit precision)
+            var out_left:  i17 = 0;
+            var out_right: i17 = 0;
+
+            inline for (0..8) |f| {
+                const idx = (e.history_idx + f) % 8;
+                
+                const samp_left:  i16 = f64_to_i16(e.history_left[idx])  >> 1;
+                const samp_right: i16 = f64_to_i16(e.history_right[idx]) >> 1;
+
+                var fir_res_left:  i17 = @truncate(@as(i32,  samp_left) * @as(i32, e.fir[f]) >> 6);
+                var fir_res_right: i17 = @truncate(@as(i32, samp_right) * @as(i32, e.fir[f]) >> 6);
+
+                if (f == 7) {
+                    fir_res_left  = trunc_i16(i17, fir_res_left);
+                    fir_res_right = trunc_i16(i17, fir_res_right);
+                }
+                
+                out_left  +%= fir_res_left;
+                out_right +%= fir_res_right;
+
+                if (f == 6) {
+                    out_left  = trunc_i16(i17, out_left);
+                    out_right = trunc_i16(i17, out_right);
+                }
+            }
+
+            out_left  = clamp_i16(i17, out_left)  & ~@as(i17, 1);
+            out_right = clamp_i16(i17, out_right) & ~@as(i17, 1);
+
+            const fb_left:  i16 = @truncate(@as(i32, out_left)  * @as(i32, e.feedback) >> 7);
+            const fb_right: i16 = @truncate(@as(i32, out_right) * @as(i32, e.feedback) >> 7);
+
+            out_left  = trunc_i16(i17, @intCast(@as(i32, out_left)  * self.evol_left  >> 7));
+            out_right = trunc_i16(i17, @intCast(@as(i32, out_right) * self.evol_right >> 7));
+
+            e.out_left[i]  = i16_to_f64(@intCast(out_left));
+            e.out_right[i] = i16_to_f64(@intCast(out_right));
+            
+            var left:  i17 = @intCast(e_sum_left);  
+            var right: i17 = @intCast(e_sum_right);
+            left  +%= @as(i17,  fb_left);
+            right +%= @as(i17, fb_right);
+
+            // Store into output buffer, to be written back to the echo buffer
+            const write_left:  i16 = @truncate(clamp_i16(i17, left)  & ~@as(i17, 1));
+            const write_right: i16 = @truncate(clamp_i16(i17, right) & ~@as(i17, 1));
+            e.write_left[i]  = i16_to_f64(write_left);
+            e.write_right[i] = i16_to_f64(write_right);
+
+            var dac_left_i17:  i17 = @intCast(sum_left);
+            var dac_right_i17: i17 = @intCast(sum_right);
+            dac_left_i17  +%= out_left;
+            dac_right_i17 +%= out_right;
+
+            self.dac_left[i]  = i16_to_f64(@intCast(clamp_i16(i17, dac_left_i17)));
+            self.dac_right[i] = i16_to_f64(@intCast(clamp_i16(i17, dac_right_i17)));
+        }
+    }
+
+    pub inline fn prepare_next_output(self: *Pipeline2) void {
+        var e = &self.echo;
+
+        e._cur_esa = e.esa;
+
+        // Post-output: Prepare echo parameters for next pipeline cycle
+        const read_head = e.read_head;
+
+        if (e.read_head == 0) {
+            e._cur_edl = e.delay;
+        }
+
+        e.read_head +%= 2;
+        if (e.read_head >= @as(u32, e._cur_edl) << 10) {
+            e.read_head = 0;
+        }
+
+        // Write back to buffer
+        if (!e.readonly) {
+            const buffer_index = (self.esa_base_index() +% read_head) % 0x8000;
+            e.shadow_buffer_a[buffer_index]      = e.write_left[0];
+            e.shadow_buffer_a[buffer_index +% 1] = e.write_right[0];
         }
     }
 
@@ -162,6 +342,26 @@ pub const Pipeline2 = struct {
             f64_to_i16(self.dac_left[index]),
             f64_to_i16(self.dac_right[index])
         };
+    }
+
+    inline fn edl_aram_len(self: *const Pipeline2) u16 {
+        const e = &self.echo;
+        return @as(u16, e._cur_edl) * 0x800;
+    }
+
+    inline fn edl_buffer_len(self: *const Pipeline2) u32 {
+        const e = &self.echo;
+        return @as(u32, e._cur_edl) * 0x400;
+    }
+
+    inline fn esa_base_aram(self: *const Pipeline2) u16 {
+        const e = &self.echo;
+        return @as(u16, e._cur_esa) * 0x100;
+    }
+
+    inline fn esa_base_index(self: *const Pipeline2) u32 {
+        const e = &self.echo;
+        return @as(u32, e._cur_esa) * 0x80;
     }
 
     fn check_settings(self: *Pipeline2) void {
